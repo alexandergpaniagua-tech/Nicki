0a2a46fcc81f8f55cf62468a403d3a855f4867ff
alexandergpaniagua-tech <alexandergpaniagua@gmail.com>
2026-06-29 19:28:47 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("card1").innerHTML = 1;
+document.getElementById("card2").innerHTML = 2;
+document.getElementById("card3").innerHTML = 3;
+document.getElementById("card4").innerHTML = 4;
+document.getElementById("card5").innerHTML = 5;
