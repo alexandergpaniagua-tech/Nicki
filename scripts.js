eb51b378b51f0e60eabca6b822bc630006f93ecc
alexandergpaniagua-tech <alexandergpaniagua@gmail.com>
2026-07-21 19:48:19 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var inputElement = document.getElementById("default");
-var pick = null;
-document.getElementById("option").value = pick;
-inputElement.addEventListener(MouseEvent.CLICK, function update() {
-document.getElementById("card1").innerHTML = pick;
-document.getElementById("card2").innerHTML = pick-1;
-document.getElementById("card3").innerHTML = pick-2;
-document.getElementById("card4").innerHTML = pick-3;
-document.getElementById("card5").innerHTML = pick-4; };
